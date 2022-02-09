8bf70d47c7595592bb895bc66cb46703b66dc460
Wekesa59 <60718340+Wekesa59@users.noreply.github.com>
2022-02-08 20:38:37 -0500

    web dev work

---
+++ CW/1301_hw1.py
@@ -0,0 +1,13 @@
+fname = input("First name: ")
+lname = input("Last name: ")
+month_num = eval(input("Please input numeric of the month "))
+month = ["Do better, choose a different number...","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+day = input("Day: ")
+year = input("Year: ")
+
+print(""+ fname + " " + lname + " was born " + month[month_num] + " " + day +", " + year )
+
+# 1) B
+# 2) D
+# 3) E
+# 4) C
